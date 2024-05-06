aa64d0c0af2ccb716dde3d86ac806a260b315ab8
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-05-06 10:51:08 +0600

    Initial Commit

---
+++ SOLID_MultipleType_TableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID_MultipleType_TableViewCell
+//
+//  Created by ZEUS on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
